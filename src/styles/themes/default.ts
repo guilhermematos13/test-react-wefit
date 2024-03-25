d99cee143840d5d17c02ac892980c8d928e7dc20
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2024-03-25 19:07:09 -0300

    chore: config styles for styled-components and global css

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  blue: '#009EDD',
+  blueHover: '#1183af',
+  green: '#039B00',
+  greenHover: '#057701',
+  backgroundColor: '#2F2E41',
+  gray: '#999999',
+} as const
