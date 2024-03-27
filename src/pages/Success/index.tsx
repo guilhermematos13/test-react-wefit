3d7e14e7ac6e65254d3531dffe70a7ebd2345400
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2024-03-27 00:18:58 -0300

    chore: creating success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,7 +1,19 @@
+import { Status } from "@components/Status";
+import SuccessPagePerson from "@assets/SuccessPagePerson.svg";
+
+import * as S from "./styles";
+import { useNavigate } from "react-router-dom";
+
 export function Success() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Success</h1>
-    </div>
+    <S.SuccessContainer>
+      <Status
+        imageUrl={SuccessPagePerson}
+        text="Compra realizada com sucesso!"
+        onClick={() => navigate("/")}
+      />
+    </S.SuccessContainer>
   );
 }
